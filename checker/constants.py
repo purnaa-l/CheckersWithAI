f28f2fd53ef6e9ee671d368652d1bd74c83e94f3
purnaa-l <mynameispurnaa@gmail.com>
2024-12-15 01:16:26 +0530

    feat: changed board colors

---
+++ checker/constants.py
@@ -0,0 +1,33 @@
+# import pygame
+
+# WIDTH, HEIGHT=800, 800
+# ROWS, COLS=8, 8
+# SQUARE_SIZE=WIDTH//COLS
+
+# #RGB CODES
+# PINK=(225, 204, 229)
+# WHITE=(255,255,255)
+# BLACK=(0, 0, 0)
+# GREY=(128,128,128)
+# YELLOW=(255, 215, 0)
+
+# CROWN=pygame.transform.scale(pygame.image.load('assets/crown.png'), (44,25))
+
+import pygame
+
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+# rgb
+PURPLE=(125, 82, 156)
+
+RED = (255, 0, 0)
+PINK=(225, 204, 229)
+WHITE = (255, 255, 255)
+YELLOW=(255, 215, 0)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREY = (225, 204, 229)
+
+CROWN = pygame.transform.scale(pygame.image.load('assets/crown.png'), (44, 25))
